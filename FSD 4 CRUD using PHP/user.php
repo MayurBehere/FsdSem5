e4df6a5d27728797cb8a23b55ffee81dce621b61
Mayur <mayurbehere7432@gmail.com>
2023-11-16 23:53:43 +0530

    User form added

---
+++ FSD 4 CRUD using PHP/user.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crud Operations</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container my-5">
+        <form method>
+            <div class="form-group py-3" >
+                <label >Name</label>
+                <input type="text" class="form-control" placeholder="Enter Name" name="name" >
+            </div>
+            <div class="form-group py-3">
+                <label >Email</label>
+                <input type="Email" class="form-control" placeholder="Enter Email" name="Email">
+            </div>
+            <div class="form-group py-3">
+                <label >Mobile</label>
+                <input type="int" class="form-control" placeholder="Enter Contact" name="Mobile">
+            </div>
+            <div class="form-group py-3">
+                <label >Password</label>
+                <input type="password" class="form-control" placeholder="Enter Password" name="Password">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>   
+</body>
+</html>
